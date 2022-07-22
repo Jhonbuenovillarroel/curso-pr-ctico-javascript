173bc805047a5d3b3132b6429c42c7a08d415e30
Jhonbueno <jhonadelbuenovillarroel@gmail.com>
2022-07-22 17:21:40 -0500

    Moficicación

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
+const numbers = document.querySelector("#amountNumbers");
+const create = document.querySelector("#createBoxes");
+const remove = document.querySelector("#removeBoxes")
+const label = document.querySelector(".labelForm")
+const inputCalc = document.querySelector("#calcular");
+const result = document.querySelector(".result");
+
 function removerInputs() {
-   input.remove()
+   for(i = 0; i < 10000000000000; i++) {
+      const input = document.querySelector(".casilla");
+      input.remove()
+   }
 }
 
 function crearInputs() {
    for(i = 0; i < parseInt(numbers.value); i++) {
       const input = document.createElement("input");
-      input.classList.add("casilla")
+      let classNumber = "casilla" + i;
+      input.classList.add(classNumber)
       label.appendChild(input);
    }
 }
@@ -24,15 +35,22 @@ function encontrarNumeroMayor(array) {
 
 }
 
-
-
-const numbers = document.querySelector("#amountNumbers");
-const create = document.querySelector("#createBoxes");
-const remove = document.querySelector("#removeBoxes")
-const label = document.querySelector(".labelForm")
-const input = document.querySelector(".casilla");
-
+function calcularNumeroMayor() {
+   const array = [];
+   for(i = 0; i < 500; i++) {
+      let classNumber = ".casilla" + i;
+      const input = document.querySelector(classNumber);
+      let value = input.value;
+      value = parseInt(value);
+      array.push(value);
+   }
+   const numeroMayor = encontrarNumeroMayor(array);
+   result.innerText = numeroMayor;
+}
 
 create.addEventListener("click", crearInputs);
 remove.addEventListener("click", removerInputs);
+inputCalc.addEventListener("click", calcularNumeroMayor);
+
+
 
