449a48d82b3b2d9a1c5722891829e0fcb262ccb3
Jhonbueno <jhonadelbuenovillarroel@gmail.com>
2022-07-22 16:08:56 -0500

    Cambios a main.js e index.html

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+function removerInputs() {
+   input.remove()
+}
+
+function crearInputs() {
+   for(i = 0; i < parseInt(numbers.value); i++) {
+      const input = document.createElement("input");
+      input.classList.add("casilla")
+      label.appendChild(input);
+   }
+}
 
 
+function encontrarNumeroMayor(array) {
+   let numeroMayor = array[0];
+   for(i = 0; i < array.length; i++) {
+      if(numeroMayor < array[i]) {
+         numeroMayor = array[i];
+      } else {
+         numeroMayor = numeroMayor;
+      }
+   }
+   return numeroMayor;
+
+}
+
+
+
+const numbers = document.querySelector("#amountNumbers");
+const create = document.querySelector("#createBoxes");
+const remove = document.querySelector("#removeBoxes")
+const label = document.querySelector(".labelForm")
+const input = document.querySelector(".casilla");
+
+
+create.addEventListener("click", crearInputs);
+remove.addEventListener("click", removerInputs);
 
